52554e65504403bcc5072d403ff9122aa9db2917
krhu <krhu1994@hotmail.com>
2019-10-22 22:50:48 -0400

    ChoicesReenabled

--- Arduino/SensorLogic/SensorLogic.ino
+++ Arduino/SensorLogic/SensorLogic.ino
@@ -10,14 +10,19 @@ GND             GND          GND
 RST             D9           D8
 3.3V            3.3V         3.3V
 */
-//#define  Finger_RST_Pin     24
-//#define  Finger_WAKE_Pin    23
+
+/*#define  Finger_RST_Pin     24
+#define  Finger_WAKE_Pin    23*/
+
 /* Include the standard Arduino SPI library */
 #include <SPI.h>
 /* Include the RFID library */
 #include <RFID.h>
 #include "finger.h"
 #include <Servo.h>
+#include "Keypad.h"
+#include "EEPROM.h"
+#include "LiquidCrystal.h"
 /* Define the DIO used for the SDA (SS) and RST (reset) pins. */
 #define SDA_DIO 9
 #define RESET_DIO 8
@@ -32,6 +37,29 @@ void RFIDloop(void);
 void Servoloop(int dir);
 void Bluetoothsetup(void);
 void Bluetoothloop(void);
+void Keypadsetup(void);
+void Keypadloop(void);
+
+LiquidCrystal liquid_crystal_display(9,8,7,6,5,4);
+char password[4];
+char initial_password[4],new_password[4];
+int i=0;
+int unlock_motor = 10;
+int incorrect_pswd = 11;
+char key_pressed=0;
+const byte rows = 4; 
+const byte columns = 3; 
+char hexaKeys[rows][columns] = {
+  {'1','2','3'},
+  {'4','5','6'},
+  {'7','8','9'},
+  {'*','0','#'}
+};
+
+byte row_pins[rows] = {A1, A6, A5, A3};
+byte column_pins[columns] = {A2, A0, A4};
+
+Keypad keypad_key = Keypad( makeKeymap(hexaKeys), row_pins, column_pins, rows, columns);
 
 Servo myservo;
 
@@ -53,6 +81,7 @@ void setup()
  Fingerprintsetup();
  RFIDsetup();
  Bluetoothsetup();
+ Keypadsetup();
 }
 
 void loop()
@@ -60,10 +89,15 @@ void loop()
   Serial.println("Which sensor do you want to use?");
   Serial.println("1 for RFID");
   Serial.println("2 for Fingerprint");
+  Serial.println("3 for Bluetooth");
+  Serial.println("4 for Keypad");
+    
+  while(Serial.available()<1){
 
-  
- 
-    choice = '1';
+  }
+
+
+    choice = Serial.read();
     Serial.print("You chose: ");
     Serial.println(choice);
     Serial.println("Press the RST button to pick again.");
@@ -86,6 +120,13 @@ void loop()
       while(1)
       {
         Bluetoothloop();
+      }
+    }
+    else if((char)choice == '4')
+    {
+      while(1)
+      {
+        Keypadloop();
       }
     }
 }
@@ -198,4 +239,147 @@ void Bluetoothloop()
   }
   if(Serial.available())
     Serial3.write(Serial.read());
+}
+//----------------------------------------------------------------------------------
+
+
+
+void Keypadsetup()
+{
+  pinMode(unlock_motor, OUTPUT);
+  pinMode(incorrect_pswd, OUTPUT);
+  Serial.println("Keyless Entry");
+  //liquid_crystal_display.begin(16,2);
+  //liquid_crystal_display.print("UCF Knights");
+  //liquid_crystal_display.setCursor(0,1);
+  //liquid_crystal_display.print("Keyless Entry");
+  delay(2000);
+  //liquid_crystal_display.clear();
+  Serial.println("Enter Password");
+  //liquid_crystal_display.print("Enter Password");
+  //liquid_crystal_display.setCursor(0,1);
+  initialpassword();
+}
+
+void Keypadloop()
+{
+  digitalWrite(unlock_motor, LOW);
+  digitalWrite(incorrect_pswd, LOW);
+  key_pressed = keypad_key.getKey();
+  if(key_pressed=='#')
+    change();
+  if (key_pressed)
+  {
+    password[i++]=key_pressed;
+    Serial.println(key_pressed);
+    //liquid_crystal_display.print(key_pressed);
+      }
+  if(i==4)
+  {
+    delay(200);
+    for(int j=0;j<4;j++)
+      initial_password[j]=EEPROM.read(j);
+    if(!(strncmp(password, initial_password,4)))
+    {
+      //liquid_crystal_display.clear();
+      Serial.println("Password Accepted");
+      //liquid_crystal_display.print("Pass Accepted");
+      digitalWrite(unlock_motor, HIGH);
+      Servoloop(0);
+
+      delay(2000);
+      //liquid_crystal_display.setCursor(0,1);
+      Serial.println("Press # to change");
+      //liquid_crystal_display.print("Pres # to change");
+      delay(2000);
+      //liquid_crystal_display.clear();
+      Serial.println("Enter Password");
+      //liquid_crystal_display.print("Enter Password:");
+      //liquid_crystal_display.setCursor(0,1);
+      i=0;
+    }
+    else
+    {
+      digitalWrite(unlock_motor, LOW);
+      //liquid_crystal_display.clear();
+      Serial.println("Wrong Password");
+      //liquid_crystal_display.print("Wrong Password");
+      digitalWrite(incorrect_pswd, HIGH);
+      //liquid_crystal_display.setCursor(0,1);
+      Serial.println("Press # to change");
+      //liquid_crystal_display.print("Pres # to Change");
+      delay(2000);
+      //liquid_crystal_display.clear();
+      Serial.println("Enter Password");
+      //liquid_crystal_display.print("Enter Password");
+      //liquid_crystal_display.setCursor(0,1);
+      i=0;
+    }
+  }
+}
+
+void change()
+{
+  int j=0;
+  //liquid_crystal_display.clear();
+  Serial.println("Current Password");
+  //liquid_crystal_display.print("Current Password");
+  //liquid_crystal_display.setCursor(0,1);
+  while(j<4)
+  {
+    char key=keypad_key.getKey();
+    if(key)
+    {
+      new_password[j++]=key;
+      Serial.println(key);
+      //liquid_crystal_display.print(key);
+    }
+    key=0;
+  }
+  delay(500);
+  if((strncmp(new_password, initial_password, 4)))
+  {
+    //liquid_crystal_display.clear();
+    Serial.println("Wrong Password");
+    //liquid_crystal_display.print("Wrong Password");
+    //liquid_crystal_display.setCursor(0,1);
+    Serial.println("Try Again");
+    //liquid_crystal_display.print("Try Again");
+    delay(1000);
+  }
+  else
+  {
+    j=0;
+    //liquid_crystal_display.clear();
+    Serial.println("New Password");
+    //liquid_crystal_display.print("New Password:");
+    //liquid_crystal_display.setCursor(0,1);
+    while(j<4)
+    {
+      char key=keypad_key.getKey();
+      if(key)
+      {
+        initial_password[j]=key;
+        Serial.println(key);
+        //liquid_crystal_display.print(key);
+        EEPROM.write(j,key);
+        j++;
+      }
+    }
+    Serial.println("Password Changed");
+    //liquid_crystal_display.print("Pass Changed");
+    delay(1000);
+  }
+  //liquid_crystal_display.clear();
+  Serial.println("Enter Password");
+  //liquid_crystal_display.print("Enter Password");
+  //liquid_crystal_display.setCursor(0,1);
+  key_pressed=0;
+}
+
+void initialpassword(){
+  for(int j=0;j<4;j++)
+    EEPROM.write(j, j+49);
+  for(int j=0;j<4;j++)
+    initial_password[j]=EEPROM.read(j);
 }
